2ef97827107abe8f62e273b84ab0f29f3dec1736
Arman Ameri <arman.ameri48+Github@gmail.com>
2026-04-03 12:39:40 +0200

    added datagen

---
+++ src/main/java/com/AidanAndino/tutorialmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,24 @@
+package com.AidanAndino.tutorialmod.datagen;
+
+import com.AidanAndino.tutorialmod.TutorialMod;
+import com.AidanAndino.tutorialmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.ALEXANDRITE.get());
+        basicItem(ModItems.RAW_ALEXANDRITE.get());
+
+        basicItem(ModItems.CHISEL.get());
+        basicItem(ModItems.PIZZA.get());
+        basicItem(ModItems.COLA.get());
+        basicItem(ModItems.AURORA_ASHES.get());
+    }
+}
